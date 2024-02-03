cd125397f9fa3ae52bbfd15131cf3a155b8825ef
Harrison Muller <hrm53@picotte001.cm.cluster>
2024-02-03 16:18:29 -0500

    Updated tutorial4_qiime_commands

--- tutorial4_qiime_commands.sh
+++ tutorial4_qiime_commands.sh
@@ -32,6 +32,18 @@ qiime demux summarize \
 
 qiime tools view demux.qzv
 
+qiime dada2 denoise-single \
+  --i-demultiplexed-seqs demux.qza \
+  --p-trim-left 0 \
+  --p-trunc-len 120 \
+  --o-representative-sequences rep-seqs.qza \
+  --o-table table.qza \
+  --o-denoising-stats stats.qza
+
+qiime metadata tabulate \
+  --m-input-file stats.qza \
+  --o-visualization stats.qzv
+
 qiime quality-filter q-score \
   --i-demux demux.qza \
   --o-filtered-sequences demux-filtered.qza \
@@ -53,4 +65,116 @@ qiime deblur visualize-stats \
   --i-deblur-stats deblur-stats.qza \
   --o-visualization deblur-stats.qzv
 
+# q2cli:
+mv rep-seqs-deblur.qza rep-seqs.qza
+mv table-deblur.qza table.qza
+# Artifact API:
+table = table_deblur
+rep_seqs = rep_seqs_deblur
 
+qiime feature-table summarize \
+  --i-table table.qza \
+  --m-sample-metadata-file sample-metadata.tsv \
+  --o-visualization table.qzv
+qiime feature-table tabulate-seqs \
+  --i-data rep-seqs.qza \
+  --o-visualization rep-seqs.qzv
+
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences rep-seqs.qza \
+  --output-dir phylogeny-align-to-tree-mafft-fasttree
+
+qiime diversity core-metrics-phylogenetic \
+  --i-phylogeny phylogeny-align-to-tree-mafft-fasttree/rooted_tree.qza \
+  --i-table table.qza \
+  --p-sampling-depth 1103 \
+  --m-metadata-file sample-metadata.tsv \
+  --output-dir diversity-core-metrics-phylogenetic
+
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity diversity-core-metrics-phylogenetic/faith_pd_vector.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --o-visualization faith-pd-group-significance.qzv
+qiime diversity alpha-group-significance \
+  --i-alpha-diversity diversity-core-metrics-phylogenetic/evenness_vector.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --o-visualization evenness-group-significance.qzv
+
+qiime diversity beta-group-significance \
+  --i-distance-matrix diversity-core-metrics-phylogenetic/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-column body-site \
+  --p-pairwise \
+  --o-visualization unweighted-unifrac-body-site-group-significance.qzv
+qiime diversity beta-group-significance \
+  --i-distance-matrix diversity-core-metrics-phylogenetic/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-column subject \
+  --p-pairwise \
+  --o-visualization unweighted-unifrac-subject-group-significance.qzv
+
+qiime emperor plot \
+  --i-pcoa diversity-core-metrics-phylogenetic/unweighted_unifrac_pcoa_results.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --p-custom-axes days-since-experiment-start \
+  --o-visualization unweighted-unifrac-emperor-days-since-experiment-start.qzv
+qiime emperor plot \
+  --i-pcoa diversity-core-metrics-phylogenetic/bray_curtis_pcoa_results.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --p-custom-axes days-since-experiment-start \
+  --o-visualization bray-curtis-emperor-days-since-experiment-start.qzv
+
+qiime diversity alpha-rarefaction \
+  --i-table table.qza \
+  --i-phylogeny phylogeny-align-to-tree-mafft-fasttree/rooted_tree.qza \
+  --p-max-depth 4000 \
+  --m-metadata-file sample-metadata.tsv \
+  --o-visualization alpha-rarefaction.qzv
+
+wget \
+  -O 'gg-13-8-99-515-806-nb-classifier.qza' \
+  'https://docs.qiime2.org/2021.11/data/tutorials/moving-pictures-usage/gg-13-8-99-515-806-nb-classifier.qza'
+
+qiime feature-classifier classify-sklearn \
+  --i-classifier gg-13-8-99-515-806-nb-classifier.qza \
+  --i-reads rep-seqs.qza \
+  --o-classification taxonomy.qza
+qiime metadata tabulate \
+  --m-input-file taxonomy.qza \
+  --o-visualization taxonomy.qzv
+
+qiime taxa barplot \
+  --i-table table.qza \
+  --i-taxonomy taxonomy.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --o-visualization taxa-bar-plots.qzv
+
+qiime feature-table filter-samples \
+  --i-table table.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --p-where '[body-site]='"'"'gut'"'"'' \
+  --o-filtered-table gut-table.qza
+
+qiime composition add-pseudocount \
+  --i-table gut-table.qza \
+  --o-composition-table comp-gut-table.qza
+
+qiime composition ancom \
+  --i-table comp-gut-table.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-column subject \
+  --o-visualization ancom-subject.qzv
+
+qiime taxa collapse \
+  --i-table gut-table.qza \
+  --i-taxonomy taxonomy.qza \
+  --p-level 6 \
+  --o-collapsed-table gut-table-l6.qza
+qiime composition add-pseudocount \
+  --i-table gut-table-l6.qza \
+  --o-composition-table comp-gut-table-l6.qza
+qiime composition ancom \
+  --i-table comp-gut-table-l6.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-column subject \
+  --o-visualization l6-ancom-subject.qzv
